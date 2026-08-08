6821aa2c9c6163bd0b3178a29e6c6c5c1bfc7361
jmarino13 <Jude@lmgmedicine.com>
2026-08-08 01:49:51 -0500

    Update start.js

--- api/qualiphy/start.js
+++ api/qualiphy/start.js
@@ -5,5 +5,5 @@
 "email":"",
 "phone_number":"",
 "tele_state":"LA",
-"webhook_url":"https://gulfcoastmeds.com/api/qualiphy/webhook"
+webhook_url: "https://gulf-coast-api.vercel.app/api/qualiphy/webhook"
 }
