c1faebfabc48ba726490f9ebcbe2bdd1897569b5
jmarino13 <Jude@lmgmedicine.com>
2026-08-08 01:54:24 -0500

    Delete api/qualiphy directory

--- api/qualiphy/start.js
+++
@@ -1,9 +0,0 @@
-{
-"exams":[3388,4226],
-"first_name":"",
-"last_name":"",
-"email":"",
-"phone_number":"",
-"tele_state":"LA",
-webhook_url: "https://gulf-coast-api.vercel.app/api/qualiphy/webhook"
-}
